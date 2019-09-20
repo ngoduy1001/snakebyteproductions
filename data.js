bbbc2551c406e4a922583f0760c9ca0a8568a3f0
Duy Ngo <dkn019@ucsd.edu>
2019-09-20 16:06:42 -0700

    data.js

--- data.js
+++ data.js
@@ -1,49 +1,18 @@
-var codeBlock = '<div class="shop-item">' 
-+ '<span class="shop-item-title"> + Album 5</span>'
-+ '<img class="shop-item-image" src="Images/Album 4.png">'
-+ '<div class="shop-item-details">'
-+ '<span class="shop-item-price">$192.99</span>'
-+ '<button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>'
-+ '</div>'
-+ '</div>'
-document.write(codeBlock);
-document.write(codeBlock);
-var concac = `
-<section class="container content-section"> + ${cac}
-<h2 class="section-header" id = "MUSIC">MUSIC</h2>
-<div class="shop-items">
-    <div class="shop-item">
-        <span class="shop-item-title">Album 1</span>
-        <img class="shop-item-image" src="Images/Album 1.png">
-        <div class="shop-item-details">
-            <span class="shop-item-price">$12.99</span>
-            <button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
+var sectionHeader = "MUSIC"
+var ItemTitle = "Album 1"
+var imgSrc = "Images/Album 1.png"
+var itemPrice = "13299"
+var code = `<section class="container content-section">
+    <h2 class="section-header" id = "${sectionHeader}">${sectionHeader}</h2>
+    <div class="shop-items">
+        <div class="shop-item">
+            <span class="shop-item-title">${ItemTitle}</span>
+            <img class="shop-item-image" src="${imgSrc}">
+            <div class="shop-item-details">
+                <span class="shop-item-price">$${itemPrice}</span>
+                <button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
+            </div>
         </div>
     </div>
-    <div class="shop-item">
-        <span class="shop-item-title">Album 2</span>
-        <img class="shop-item-image" src="Images/Album 2.png">
-        <div class="shop-item-details">
-            <span class="shop-item-price">$14.99</span>
-            <button class="btn btn-primary shop-item-button"type="button">ADD TO CART</button>
-        </div>
-    </div>
-    <div class="shop-item">
-        <span class="shop-item-title">Album 3</span>
-        <img class="shop-item-image" src="Images/Album 3.png">
-        <div class="shop-item-details">
-            <span class="shop-item-price">$9.99</span>
-            <button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
-        </div>
-    </div>
-    <div class="shop-item">
-        <span class="shop-item-title">Album 4</span>
-        <img class="shop-item-image" src="Images/Album 4.png">
-        <div class="shop-item-details">
-            <span class="shop-item-price">$19.99</span>
-            <button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
-        </div>
-    </div>
-    <!-- <script src ="data.js"></script> -->
-</div>
 </section>`
+//document.write(code)
