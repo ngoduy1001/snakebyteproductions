36700708de1c6ae45107e7259c57f19ad23defd4
Duy Ngo <dkn019@ucsd.edu>
2019-10-19 20:50:23 -0700

    deploy to firebase

--- data.js
+++ data.js
@@ -1,13 +1,15 @@
-var sectionHeader = "MUSIC"
-var ItemTitle = "Album 1"
-var imgSrc = "Images/Album 1.png"
+/* Manually add items onto page */
+
+var sectionHeader = "concac"
+var ItemTitle = "David Bitch"
+var imgSrc = "images/banner.jpg"
 var itemPrice = "13299"
 var code = `<section class="container content-section">
     <h2 class="section-header" id = "${sectionHeader}">${sectionHeader}</h2>
     <div class="shop-items">
         <div class="shop-item">
             <span class="shop-item-title">${ItemTitle}</span>
-            <img class="shop-item-image" src="${imgSrc}">
+            <img class="shop-item-image" src="${imgSrc}"> 
             <div class="shop-item-details">
                 <span class="shop-item-price">$${itemPrice}</span>
                 <button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
@@ -15,4 +17,6 @@ var code = `<section class="container content-section">
         </div>
     </div>
 </section>`
-//document.write(code)
+document.write(code)
+document.write(code)
+document.write(code)
